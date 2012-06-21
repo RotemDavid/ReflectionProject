a0e1512568e162a2b4ceab5b89decd2a315ab4a5
Sam Stern <hatboysam@gmail.com>
2012-06-21 12:40:00 -0400

    Cleaned up Javascript (collapser) and XSLT

--- public/javascripts/collapser.js
+++ public/javascripts/collapser.js
@@ -3,7 +3,9 @@
 $(document).ready(function() {
 
 
-    //Callback should take one argument
+    //Link - the link from which to request XML
+    //Callback - the function to handle the XML on return, should take
+        //a single argument (an XML doc)
     function ajax(link, callback) {
         var xmlhttp = null;
         xmlhttp = new XMLHttpRequest();
@@ -16,36 +18,25 @@ $(document).ready(function() {
         }
     }
 
-    function styleXMLSpecial(xmlLoc, xslLoc, divID) {
+    //Style an XML file with XSL, append to a div
+    //xmlLoc - link to the XML file
+    //xslLoc - link to the XSLT sheet (should be *.xml file)
+    //divID - the target div for the content (will be emptied before append)
+    //callback - a zero argument function to be executed upon task completion
+    function styleXML(xmlLoc, xslLoc, divID, callback) {
         styledXML = ajax(xslLoc, function(xsl) {
             ajax(xmlLoc, function(xml) {
-                //Clear the div
                 $(divID).empty();
                 xsltProcessor = new XSLTProcessor();
                 xsltProcessor.importStylesheet(xsl);
                 styledXml = xsltProcessor.transformToFragment(xml, document);
                 $(divID).append(styledXml);
-                //Auto hide all
-                $("li div.btn").each(divClick);    
+                callback();   
             })
         });
     }
 
-    function styleXML(xmlLoc, xslLoc, divID) {
-        styledXML = ajax(xslLoc, function(xsl) {
-            ajax(xmlLoc, function(xml) {
-                //Clear the div
-                $(divID).empty();
-                xsltProcessor = new XSLTProcessor();
-                xsltProcessor.importStylesheet(xsl);
-                styledXml = xsltProcessor.transformToFragment(xml, document);
-                $(divID).append(styledXml);  
-            })
-        });
-    }
-
-    styleXMLSpecial("/assets/static/newxmlfiles/summary.xml", "/assets/static/sidebarstyle.xml", "#sidebar");
-
+    //Collapse a nested list when a button (represented by a <div> tag) is clicked
     function divClick() {
         $(this).parent().children(".folders").each(function() {
             $(this).slideToggle('fast');
@@ -57,9 +48,18 @@ $(document).ready(function() {
         $(this).toggleClass('open');
     }
 
+    //Collapses all of the paths
+    function collapseAll() {
+        $("li div.btn").each(divClick);
+    }
+
+    //Style the sidebar
+    styleXML("/assets/static/newxmlfiles/summary.xml", "/assets/static/sidebarstyle.xml", "#sidebar", collapseAll); 
+
     //Behavior on clicking a DIV
     $("li div.btn").live("click", divClick);
 
+    //Load an XML file for a REST path into the main window
     $("span a").live("click", function(e) {
         e.preventDefault();
         styleXML($(this).attr("href"), "/assets/xml/xmltohtml.xml", "#tester1");
